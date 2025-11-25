44dd97f5196302041d15d36922004ecf800b8ba0
Siphosethu Lokwe <sethucarter@gmail.com>
2025-11-24 18:56:49 -0800

    add db

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")  
+        policy.WithOrigins("http://localhost:5013")  
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
